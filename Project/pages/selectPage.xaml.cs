1c2573bedc15c6ecd7cae1b9dcf1b0a82417d3c4
0xe06d7363 <swiftsdk@outlook.com>
2016-11-22 19:16:53 +0300

    Переходы на другие страницы

---
+++ Project/pages/selectPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class selectPage : Window
+    {
+        public selectPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void buttonEge(object sender, RoutedEventArgs e)
+        {
+            var newForm = new egePage(); 
+            newForm.Show(); 
+            this.Close(); 
+        }
+        private void buttonSchool(object sender, RoutedEventArgs e)
+        {
+            var newForm = new schoolPage();
+            newForm.Show();
+            this.Close();
+        }
+        private void buttonOlymp(object sender, RoutedEventArgs e)
+        {
+            var newForm = new olympPage();
+            newForm.Show();
+            this.Close();
+        }
+    }
+ }
