e1f14d9f4766c325c1464e2691d15731b9e6d92b
0x452 <swiftsdk@yahoo.com>
2016-11-22 18:20:53 +0300

    Initial commit

---
+++ Project/selectPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new System.Uri("egePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+        private void Button_Olymp_Clicl(object sender, RoutedEventArgs e )
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new System.Uri("egePage.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+ }
+
